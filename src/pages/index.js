963cb3dbbbd9570ccad900d39ffaa0cd31403bbf
Jacek Kawalec <jokerchocolate@gmail.com>
2021-10-07 17:26:31 +0200

    Costa Concordia

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Dig into the mine of fascinating ideas.</p>
+      {/* <p>Tasty recipes, thoughts about movies and programing
+        technologies simply explained You can find on<Link to="/blog">my blog</Link>
+      </p>
+      <p>Some of my <Link to="/apps">Apps</Link></p>
+      <p>And description of my <Link to="/way">Programming Journey</Link></p>
+      <p>List of blog posts by category<Link to="/map">Map</Link></p> */}
+      <button><Link to="/map">Explore</Link></button>
       <StaticImage
         alt="An Img from the internet"
         src="../images/_HYHtD8F.webp"

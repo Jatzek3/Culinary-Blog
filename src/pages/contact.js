b350d84d23d9ed8fb78bab76c1990e9e9901f843
Jacek Kawalec <jokerchocolate@gmail.com>
2021-10-13 09:29:44 +0200

    added Index, About and Contact classes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,21 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {
+    contact,
+    contactHeader,
+    contactParagraph,
+} from "../css/contact.module.css"
 
 const ContactPage = () => {
     return (
         <Layout pageTitle="Contact Page">
-            <h2>This is contact page</h2>
-            <p>@ jokerchocolate@gmail.com</p>
-            <p>Github: https://github.com/Jatzek3</p>
-            <p>tel: 783 574 322 </p>
-            <p>Jacek Kawalec</p>
+            <div className={contact}>
+                <h2 className={contactHeader}>This is contact page</h2>
+                <p className={contactParagraph}>@ jokerchocolate@gmail.com</p>
+                <p className={contactParagraph}>Github: https://github.com/Jatzek3</p>
+                <p className={contactParagraph}>Jacek Kawalec</p>
+            </div>
+
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,26 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import { Link } from 'gatsby'
+import {
+  intro,
+  introText,
+  introButton,
+  introButtonText,
+  introImg,
+} from "../css/indexjs.module.css"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>Dig into the mine of fascinating ideas.</p>
-      <button><Link to="/about">Explore</Link></button>
-      <StaticImage
-        alt="An Img from the internet"
-        src="../images/_HYHtD8F.webp"
-      />
+      <div className={intro}>
+        <p className={introText}>Dig into the mine of fascinating ideas.</p>
+        <button className={introButton}><Link classname={introButtonText} to="/about">Explore</Link></button>
+        <StaticImage className={introImg}
+          alt="An Img from the internet"
+          src="../images/_HYHtD8F.webp"
+        />
+      </div>
+
     </Layout>
   )
 }

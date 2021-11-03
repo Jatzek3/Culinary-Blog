2af0932376fe25157fdea00bf84ac0ad1a3a4e43
Jacek Kawalec <jokerchocolate@gmail.com>
2021-11-03 17:10:35 +0100

    Map and Contact ok

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,16 +4,21 @@ import {
     contact,
     contactHeader,
     contactParagraph,
+    contactInfo,
+    textParagraphLink
 } from "../css/contact.module.css"
 
 const ContactPage = () => {
     return (
         <Layout pageTitle="Contact Page">
             <div className={contact}>
-                <h2 className={contactHeader}>This is contact page</h2>
-                <p className={contactParagraph}>@ jokerchocolate@gmail.com</p>
-                <p className={contactParagraph}>Github: https://github.com/Jatzek3</p>
-                <p className={contactParagraph}>Jacek Kawalec</p>
+                <div className={contactInfo}>
+                    <h2 className={contactHeader}>Lets get in touch!</h2>
+                    <p className={contactParagraph}>@ jokerchocolate@gmail.com</p>
+                    <p className={contactParagraph}><a href="https://github.com/Jatzek3"
+                        className={textParagraphLink} target="blank">Github: https://github.com/Jatzek3</a></p>
+                    <p className={contactParagraph}>Jacek Kawalec</p>
+                </div>
             </div>
 
         </Layout>

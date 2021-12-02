c81b754e782087dbfb241e6613b61bc5911bcf7a
Jacek Kawalec <jokerchocolate@gmail.com>
2021-12-02 13:47:20 +0100

    added favicn

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,20 +3,16 @@ import { Link, useStaticQuery, graphql } from 'gatsby'
 import MobileNavigation from "./navbar/MobileNavigation"
 import Navigation from "./navbar/Navigation"
 import {
-    container,
-    heading,
-    // navLinks,
-    // navLinkItem,
-    // navLinkText,
-    siteTitle,
-    mainbar,
-    // site,
+  container,
+  heading,
+  siteTitle,
+  mainbar,
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
 
 
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
@@ -26,20 +22,20 @@ const Layout = ({ pageTitle, children }) => {
     }
   `)
 
-    return (
-        <div className={container}>
-            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <div className={mainbar}>
-                <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-                <MobileNavigation />
-                <Navigation />
-            </div>
-            <h1 className={heading}>{pageTitle}</h1>
-            <main>
-                {children}
-            </main>
-        </div >
-    )
+  return (
+    <div className={container}>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <div className={mainbar}>
+        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+        <MobileNavigation />
+        <Navigation />
+      </div>
+      <h1 className={heading}>{pageTitle}</h1>
+      <main>
+        {children}
+      </main>
+    </div >
+  )
 }
 
 export default Layout

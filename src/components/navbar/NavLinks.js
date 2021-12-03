4a6cabecb98f1e8685e8788108d0644551d80234
Jacek Kawalec <jokerchocolate@gmail.com>
2021-12-03 22:28:21 +0100

    added underline for active page

--- src/components/navbar/NavLinks.js
+++ src/components/navbar/NavLinks.js
@@ -4,43 +4,51 @@ import {
     navLinks,
     navLinkItem,
     navLinkText,
+    active,
 } from './navBar.module.css'
 
 const NavLinks = () => {
     return (
         <ul className={navLinks}>
             <li className={navLinkItem}>
-                <Link to="/" className={navLinkText}>
+                <Link to="/" className={navLinkText}
+                    activeStyle={{ borderRight: "1rem", borderBottom: "4px solid black" }}>
                     HOME
                 </Link>
             </li>
             <li className={navLinkItem}>
-                <Link to="/about" className={navLinkText}>
+                <Link to="/about" className={navLinkText}
+                    activeStyle={{ borderRight: "1rem", borderBottom: "4px solid black" }}>
                     ABOUT
                 </Link>
             </li>
             <li className={navLinkItem}>
-                <Link to="/blog" className={navLinkText}>
+                <Link to="/blog" className={navLinkText}
+                    activeStyle={{ borderRight: "1rem", borderBottom: "4px solid black" }}>
                     BLOG
                 </Link>
             </li>
             <li className={navLinkItem}>
-                <Link to="/apps" className={navLinkText}>
+                <Link to="/apps" className={navLinkText}
+                    activeStyle={{ borderRight: "1rem", borderBottom: "4px solid black" }}>
                     APPS
                 </Link>
             </li>
             <li className={navLinkItem}>
-                <Link to="/way" className={navLinkText}>
+                <Link to="/way" className={navLinkText}
+                    activeStyle={{ borderRight: "1rem", borderBottom: "4px solid black" }}>
                     ROAD
                 </Link>
             </li>
             <li className={navLinkItem}>
-                <Link to="/map" className={navLinkText}>
+                <Link to="/map" className={navLinkText}
+                    activeStyle={{ borderRight: "1rem", borderBottom: "4px solid black" }}>
                     MAP
                 </Link>
             </li>
             <li className={navLinkItem}>
-                <Link to="/contact" className={navLinkText}>
+                <Link to="/contact" className={navLinkText}
+                    activeStyle={{ borderRight: "1rem", borderBottom: "4px solid black" }}>
                     CONTACT
                 </Link>
             </li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
-import { Link } from 'gatsby'
+
 import {
   mainContainer,
   intro,
@@ -11,12 +12,17 @@ import {
   imgContainer,
   introImg,
   introGreeting,
+  siteBreak,
+  newestPosts,
 } from "../css/indexjs.module.css"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const newPosts = data.allMdx.nodes
+
   return (
     <Layout pageTitle="Home Page">
       <div className={mainContainer}>
+        {console.log(newPosts)}
         <div className={intro}>
           <h2 className={introGreeting}>Hey!</h2>
           <p className={introText}>Take a deep dive to surface astonishing ideas.</p>
@@ -29,8 +35,33 @@ const IndexPage = () => {
           />
         </div>
       </div>
+      <div className={siteBreak}>
+      </div>
+      <div className={newestPosts}>
+        Check out the newest blog posts:
+      </div>
     </Layout>
   )
 }
+export const query = graphql`
+  query {
+    allMdx(
+      sort: {fields: frontmatter___date, order: DESC}
+      filter: {fileAbsolutePath: {regex: "/blog/"}}
+      limit: 3
+    ) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          subtitle
+          topic
+        }
+        id
+        slug
+      }
+    }
+  }
+  `
 
 export default IndexPage
